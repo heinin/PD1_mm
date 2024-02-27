ea97b276dd30ea0aa00e9589a40b037133d3aacb
heinin <heini.natri@gmail.com>
2024-02-27 13:40:41 -0700

    Filtering, comparative analysis

--- code/utilities.R
+++ code/utilities.R
@@ -1,16 +1,3 @@
-lmp <- function (modelobject) {
-  if (class(modelobject) != "lm") stop("Not an object of class 'lm' ")
-  f <- summary(modelobject)$fstatistic
-  p <- pf(f[1],f[2],f[3],lower.tail=F)
-  attributes(p) <- NULL
-  return(p)
-}
-
-nullToNA <- function(x) {
-  x[sapply(x, is.null)] <- NA
-  return(x)
-}
-
 get_pcs <- function(seurat_object, reduction_name="pca") {
   
   # Determine percent of variation associated with each PC
@@ -33,78 +20,5 @@ get_pcs <- function(seurat_object, reduction_name="pca") {
   #pcs <- min(co1, co2)
   
   c(co1, co2)
-  
-}
-
-# TODO: move to the function, save to a file
-# Create a dataframe with values
-#plot_df <- data.frame(pct = pct, 
-#                      cumu = cumu, 
-#                      rank = 1:length(pct))
-#
-## Elbow plot to visualize 
-#ggplot(plot_df, aes(cumu, pct, label = rank, color = rank > pcs)) + 
-#  geom_text() + 
-#  geom_vline(xintercept = 90, color = "grey") + 
-#  geom_hline(yintercept = min(pct[pct > 5]), color = "grey") +
-#  theme_bw()
-
-potato_pcs <- function(seurat_object, filename, pcs){
-  # Empty matrices for the distances for the UMAP cell distances
-  potato <-  matrix(ncol = length(pcs), nrow = ncol(seurat_object))
-  potato2 <-  matrix(ncol = length(pcs), nrow = ncol(seurat_object))
-
-  j <- 0
-  for(i in pcs) {
-    j <- j + 1
-    print(i)
-    #print(j)
-    # Running UMAP with a given set of PCs
-    onion <- RunUMAP(seurat_object, dims = 1:i, verbose = T)
-    # UMAP distances between cells are stored in cell.embeddings
-    potato[, j] <- onion@reductions$umap@cell.embeddings[, 1]
-    potato2[, j] <- onion@reductions$umap@cell.embeddings[, 2]
-  }
-  
-  colnames(potato) <- pcs
-  colnames(potato2) <- pcs
-  
-  # Subsetting to a random set of cells
-  mrand_obs <- NULL
-  sample_subset <- sample(1:ncol(seurat_object), 3000)
-  #potato_new <- potato[sample(1:ncol(seurat_object), 3000), ]
-  #potato2_new <- potato2[sample(1:ncol(seurat_object), 3000), ]
-  potato_new <- potato[sample_subset, ]
-  potato2_new <- potato2[sample_subset, ]
-  
-  # Mantel's test (correlation between two distance matrices)
-  # Comparing the distances between neighboring UMAPs
-  for(i in  1:c(dim(potato_new)[2] - 1)) {
-    onion <- dist(potato_new[, i])
-    onion2 <- dist(potato_new[, i + 1])
-    onion3 <- mantel.randtest(onion, onion2)$obs
-    onion <- dist(potato2_new[, i])
-    onion2 <- dist(potato2_new[, i + 1])
-    onion4 <- mantel.randtest(onion, onion2)$obs
-    
-    #plot(r1 <- mantel.randtest(onion,onion2), main = "Mantel's test")
-    #r1
-    
-    mrand_obs <- rbind(mrand_obs, cbind(onion3, onion4))
-  }
-  
-  pdf(filename)
-  #ylim = c(0.8, 1)
-  plot(pcs[1:length(pcs)-1],
-       mrand_obs[, 1], xlab="PCs included in the UMAP", ylab="Mantel's test observed correlation",
-       ylim = c(0.2, 1.2))
-  lines(spline(x = pcs[1:length(pcs)-1],
-               y = mrand_obs[, 1]))
-  # ylim = c(0.8, 1)
-  points(pcs[1:length(pcs)-1],
-         mrand_obs[, 2], col = "red", ylim = c(0.2, 1.2))
-  lines(spline(x = pcs[1:length(pcs)-1],
-               y = mrand_obs[, 2]), col = "red")
-  dev.off()
   
 }
